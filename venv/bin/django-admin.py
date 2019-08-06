1732c0b8bdd02e59b3e7549dfe2702a6062c3502
RafaelMatos <rafael-matos@outlook.com>
2019-08-06 16:27:09 -0300

    Initial Commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mal/Documentos/Codes/Django/Django for Beginners/Chapter 5/blog/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
